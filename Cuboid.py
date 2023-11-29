68b936969bf77795fa6461ed71d430d6518f8bfa
MOHAMED SHEBIN T P <148629731+shebin-zeo@users.noreply.github.com>
2023-11-29 15:22:44 +0530

    Add files via upload

---
+++ Cuboid.py
@@ -0,0 +1,14 @@
+#area() and volume() of cuboid
+
+def area(a):
+    area=6*a**2
+    return area
+
+def volume(a):
+    volume=a**3
+    return volume
+
+
+#print(area(2))
+#print(volume(3))
+
---
+++ Sphere.py
@@ -0,0 +1,16 @@
+#area() and volume() of sphere
+
+import math
+
+def area(r):
+    area=4*math.pi*r**2
+    return area
+
+def volume(r):
+    volume=(4*math.pi*r**3)/3
+    return volume
+
+
+#print(area(2))
+#print(volume(3))
+
